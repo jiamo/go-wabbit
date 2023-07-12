33269741a171b805d6beb9d01bd7884c3e7344db
jiamo <life.130815@gmail.com>
2023-07-13 05:21:00 +0800

    add some test case

--- cmd/model/model_main.go
+++ cmd/model/model_main.go
@@ -13,5 +13,22 @@ func main() {
 
 	println(model.NodeAsSource(modelA, model.NewContext()))
 	println(model.NodeAsSource(
-		&model.PrintStatement{&model.Integer{"2"}}, model.NewContext()))
+		&model.PrintStatement{&model.Integer{"2"}},
+		model.NewContext()))
+
+	print(model.NodeAsSource(
+		&model.Statements{
+			[]model.Statement{
+				&model.ConstDeclaration{model.Name{"pi"}, nil, &model.Float{"3.14159"}},
+
+				&model.ConstDeclaration{model.Name{"tau"}, nil, &model.Mul{&model.Float{"2.0"}, &model.Name{"pi"}}},
+				&model.VarDeclaration{model.Name{"radius"}, nil, &model.Float{"4.0"}},
+				&model.VarDeclaration{model.Name{"perimeter"}, &model.NameType{"float"}, nil},
+				&model.ExpressionAsStatement{&model.Assignment{&model.Name{"perimeter"}, &model.Mul{&model.Name{"tau"}, &model.Name{"radius"}}}},
+				&model.PrintStatement{&model.Name{"perimeter"}},
+			},
+		},
+		model.NewContext(),
+	))
+
 }
--- model/model.go
+++ model/model.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"fmt"
+	"strings"
 )
 
 //type Node struct {
@@ -20,7 +21,7 @@ type NodeInfo struct {
 
 type Expression interface {
 	Node
-	expressionNode()
+	ExpressionNode()
 }
 
 //type Statement struct {
@@ -29,7 +30,7 @@ type Expression interface {
 
 type Statement interface {
 	Node
-	statementNode()
+	StatementNode()
 }
 
 var staticID = 0
@@ -54,14 +55,14 @@ type Float struct {
 	Value string
 }
 
-func (n *Float) expressionNode() {}
+func (n *Float) ExpressionNode() {}
 func (n *Float) Id() int         { return 0 }
 
 type Integer struct {
 	Value string
 }
 
-func (n *Integer) expressionNode() {}
+func (n *Integer) ExpressionNode() {}
 func (n *Integer) Id() int         { return 0 }
 
 // Type should be interface ?
@@ -73,6 +74,9 @@ type Type interface {
 type NameType struct {
 	Name string
 }
+
+func (n *NameType) Id() int      { return 0 }
+func (n *NameType) Type() string { return "" }
 
 type IntegerType struct {
 }
@@ -91,19 +95,68 @@ type UnaryOp struct {
 	Operand Expression
 }
 
+type Neg struct {
+	Operand Expression
+}
+
+func (n *Neg) ExpressionNode() {}
+func (n *Neg) Id() int         { return 0 }
+
+type Pos struct {
+	Operand Expression
+}
+
+func (n *Pos) ExpressionNode() {}
+func (n *Pos) Id() int         { return 0 }
+
 type BinOpWithOp struct {
 	Op    string
 	Left  Expression
 	Right Expression
 }
 
-func (n *BinOpWithOp) expressionNode() {}
+func (n *BinOpWithOp) ExpressionNode() {}
 func (n *BinOpWithOp) Id() int         { return 0 }
 
 type BinOp struct {
 	Left  Expression
 	Right Expression
 }
+
+func (n *BinOp) ExpressionNode() {}
+func (n *BinOp) Id() int         { return 0 }
+
+type Add struct {
+	Left  Expression
+	Right Expression
+}
+
+func (n *Add) ExpressionNode() {}
+func (n *Add) Id() int         { return 0 }
+
+type Sub struct {
+	Left  Expression
+	Right Expression
+}
+
+func (n *Sub) ExpressionNode() {}
+func (n *Sub) Id() int         { return 0 }
+
+type Mul struct {
+	Left  Expression
+	Right Expression
+}
+
+func (n *Mul) ExpressionNode() {}
+func (n *Mul) Id() int         { return 0 }
+
+type Div struct {
+	Left  Expression
+	Right Expression
+}
+
+func (n *Div) ExpressionNode() {}
+func (n *Div) Id() int         { return 0 }
 
 type RelOp struct {
 	Left  Expression
@@ -130,23 +183,34 @@ type PrintStatement struct {
 	Value Expression
 }
 
-func (n *PrintStatement) Id() int { return 0 }
+func (n *PrintStatement) StatementNode() {}
+func (n *PrintStatement) Id() int        { return 0 }
 
 type Statements struct {
 	Statements []Statement
 }
 
+func (n *Statements) Id() int { return 0 }
+
 type Name struct {
-	Value string
+	Text string
 }
+
+func (n *Name) ExpressionNode() {}
+func (n *Name) Id() int         { return 0 }
 
 type CompoundExpression struct {
 	Statements []Statement
 }
 
+func (n *CompoundExpression) Id() int { return 0 }
+
 type ExpressionAsStatement struct {
 	Expression Expression
 }
+
+func (n *ExpressionAsStatement) StatementNode() {}
+func (n *ExpressionAsStatement) Id() int        { return 0 }
 
 type Grouping struct {
 	Expression Expression
@@ -164,20 +228,32 @@ type ConstDeclaration struct {
 	Value Expression
 }
 
+func (n *ConstDeclaration) StatementNode() {}
+func (n *ConstDeclaration) Id() int        { return 0 }
+
 type VarDeclaration struct {
 	Name  Name
 	Type  Type
 	Value Expression
 }
 
+func (n *VarDeclaration) StatementNode() {}
+func (n *VarDeclaration) Id() int        { return 0 }
+
 type Assignment struct {
 	Location Expression
 	Value    Expression
 }
 
+func (n *Assignment) ExpressionNode() {}
+func (n *Assignment) Id() int         { return 0 }
+
 type Character struct {
-	Value rune
+	Value string
 }
+
+func (n *Character) ExpressionNode() {}
+func (n *Character) Id() int         { return 0 }
 
 type IfStatement struct {
 	Test        Expression
@@ -230,20 +306,80 @@ func (c *Context) NewBlock() *Context {
 }
 
 func NodeAsSource(node Node, context *Context) string {
+	indent_str := context.Indent
 	switch v := node.(type) {
 	case *Integer:
 		return v.Value
 	case *Float:
 		return v.Value
+	case *Name:
+		return v.Text
+	case *NameType:
+		return v.Name
 	case *FloatType:
 		return "float"
 	case *BinOpWithOp:
 		return fmt.Sprintf(" %s %s %s",
 			NodeAsSource(v.Left, context), v.Op,
 			NodeAsSource(v.Right, context))
-	// ... 其他类型
+	case *Add:
+		return fmt.Sprintf(" %s %s %s",
+			NodeAsSource(v.Left, context), "+",
+			NodeAsSource(v.Right, context))
+	case *Sub:
+		return fmt.Sprintf(" %s %s %s",
+			NodeAsSource(v.Left, context), "-",
+			NodeAsSource(v.Right, context))
+	case *Mul:
+		return fmt.Sprintf(" %s %s %s",
+			NodeAsSource(v.Left, context), "*",
+			NodeAsSource(v.Right, context))
+	case *Div:
+		return fmt.Sprintf(" %s %s %s",
+			NodeAsSource(v.Left, context), "/",
+			NodeAsSource(v.Right, context))
+	case *ConstDeclaration:
+		if v.Type == nil {
+			return fmt.Sprintf("%sconst %s = %s;",
+				indent_str, NodeAsSource(&v.Name, context),
+				NodeAsSource(v.Value, context))
+		} else {
+			return fmt.Sprintf("%sconst %s %s = %s;",
+				indent_str, NodeAsSource(&v.Name, context),
+				NodeAsSource(v.Type, context),
+				NodeAsSource(v.Value, context))
+		}
+	case *VarDeclaration:
+		if v.Value != nil {
+			if v.Type == nil {
+				return fmt.Sprintf("%sconst %s = %s;",
+					indent_str, NodeAsSource(&v.Name, context),
+					NodeAsSource(v.Value, context))
+			} else {
+				return fmt.Sprintf("%sconst %s %s = %s;",
+					indent_str, NodeAsSource(&v.Name, context),
+					NodeAsSource(v.Type, context),
+					NodeAsSource(v.Value, context))
+			}
+		} else {
+			return fmt.Sprintf("%sconst %s %s;",
+				indent_str, NodeAsSource(&v.Name, context),
+				NodeAsSource(v.Type, context))
+		}
+	case *Assignment: // Assignment is exp not statement
+		return fmt.Sprintf("%s = %s",
+			NodeAsSource(v.Location, context),
+			NodeAsSource(v.Value, context))
+	case *ExpressionAsStatement:
+		return fmt.Sprintf("%s;", NodeAsSource(v.Expression, context))
 	case *PrintStatement:
-		return fmt.Sprintf("print %s", NodeAsSource(v.Value, context))
+		return fmt.Sprintf("print %s;", NodeAsSource(v.Value, context))
+	case *Statements:
+		var ts []string
+		for _, s := range v.Statements {
+			ts = append(ts, NodeAsSource(s, context))
+		}
+		return strings.Join(ts, "\n")
 	default:
 		panic(fmt.Sprintf("Can't convert %v to source", v))
 	}
