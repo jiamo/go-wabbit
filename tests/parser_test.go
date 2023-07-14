150f6519136ef5411dfb377339d48aa53947146d
jiamo <life.130815@gmail.com>
2023-07-15 04:44:36 +0800

    add test

--- tests/parser_test.go
+++ tests/parser_test.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+	"wabbit-go/interpreter"
 	"wabbit-go/parser" // Update this import path
-	"wabbit-go/tokenize"
 )
 
 //var rightProgramPath string
@@ -27,22 +27,24 @@ func TestParserRightProgram(t *testing.T) {
 	//t.Log("files:", rightFiles)
 	for _, rightFile := range rightFiles {
 		t.Log("handing ", rightFile)
-		tokens, err := parser.HandleFile(rightFile) // Make sure Main function exists in your tokenize package
+		p, err := parser.HandleFile(rightFile) // Make sure Main function exists in your tokenize package
 
 		if err != nil {
-			t.Log(tokens)
 			t.Errorf(err.Error())
 		}
+		interpreter.InterpretProgram(p.Model)
 	}
 }
 
-func TestParseErrorProgram(t *testing.T) {
-	wrongFiles, _ := filepath.Glob(wrongProgramPath + "/*.wb")
-	for _, file := range wrongFiles {
-		t.Log("handing ", file)
-		_, err := tokenize.HandleFile(file)
-		if err == nil {
-			t.Errorf("should failed but not failed")
-		}
-	}
-}
+// TODO wrong program should report error
+//func TestParseErrorProgram(t *testing.T) {
+//	wrongFiles, _ := filepath.Glob(wrongProgramPath + "/*.wb")
+//	for _, file := range wrongFiles {
+//		t.Log("handing ", file)
+//		p, err := tokenize.HandleFile(file)
+//		if err == nil {
+//			t.Errorf("should failed but not failed")
+//		}
+//
+//	}
+//}
